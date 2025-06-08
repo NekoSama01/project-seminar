4b5ed07068b46cff3ff3a34026c19a46385ec17d
NekoSama01 <hamza555444552@gmail.com>
2025-06-08 15:27:12 +0700

    ลง firebase แล้วรันได้

--- fishing_guide_app/android/app/build.gradle.kts
+++ fishing_guide_app/android/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -8,7 +9,7 @@ plugins {
 android {
     namespace = "com.example.fishing_guide_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,7 +25,7 @@ android {
         applicationId = "com.example.fishing_guide_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -41,4 +42,14 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+
+  implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
+
+  implementation("com.google.firebase:firebase-analytics")
+
+  implementation("com.google.firebase:firebase-storage")
+  implementation("com.google.firebase:firebase-auth")
 }
